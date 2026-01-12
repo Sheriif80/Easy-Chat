e575ca721c0a1f3255d572bc2b6fdab6a9ce2935
Sherif80 <sherifashraf1054@gmail.com>
2026-01-12 23:20:36 +0200

    added the login function to the cubit

--- lib/cubits/cubit/login_cubit.dart
+++ lib/cubits/cubit/login_cubit.dart
@@ -1,8 +1,25 @@
 import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:meta/meta.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginInitial());
+
+  Future<void> signInUser({
+    required String email,
+    required String password,
+  }) async {
+    emit(LoginLoading());
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      emit(LoginSuccess());
+    } on Exception catch (e) {
+      emit(LoginFailure());
+    }
+  }
 }
